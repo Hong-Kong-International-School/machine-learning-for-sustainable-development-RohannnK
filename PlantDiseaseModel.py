ec2131814b44813db2e81601e291344aad7486de
Rohan Kaman <rohankaman@gmail.com>
2023-05-19 09:21:20 +0800

    Imported weights and compiled model

--- PlantDiseaseModel.py
+++ PlantDiseaseModel.py
@@ -1,14 +1,15 @@
 import numpy as np
 import tensorflow as tf
+import keras
 import os
 import cv2
-from PIL import Image
 from keras.models import Model
 from keras.layers import Dense, Flatten
 from keras.preprocessing.image import ImageDataGenerator
 from keras.models import load_model
 # tensorflow-metal = 0.5.0, tensorflow-macos = 2.9
-my_new_model = load_model('/Users/rohankaman/Desktop/PlantDiseaseModel-v1.h5', compile = False)
-test_image = tf.keras.utils.image.load('/Users/rohankaman/Desktop/Plant Dataset/test/test/AppleCedarRust4.JPG')
-result = my_new_model.predict(test_image)
-print(result)
+WEIGHTS_PATH = 'PlantDiseaseModel-v1.h5'
+plant_model = tf.keras.models.load_model(WEIGHTS_PATH, compile = False)
+plant_model.compile(loss= tf.keras.losses.CategoricalCrossentropy(),
+    optimizer= tf.keras.optimizers.Adam(learning_rate=0.001),
+    metrics= ['accuracy'])
