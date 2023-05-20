0fb5dd970f56d1dfb68b8dffc5b76fa8840ec4f0
Rohan Kaman <rohankaman@gmail.com>
2023-05-20 17:49:15 +0800

    fixed coreml issues

--- convert.py
+++ convert.py
@@ -1,4 +1,7 @@
-import coremltools as ct
 import tensorflow as tf
+import coremltools as ct
 
-ml_model = ct.convert('PlantDiseaseModel-v1.h5', convert_to="mlprogram")
+plant_model = tf.keras.models.load_model('PlantDiseaseModel-v1.h5', compile = False)
+
+ml_model = ct.convert(plant_model, source = 'tensorflow', convert_to="mlprogram")
+ml_model.save('/Users/rohankaman/Documents/GitHub/machine-learning-for-sustainable-development-RohannnK/coreml_model.mlpackage')
